4313e8da6fb3ffb57e853d7b2d1dfb2a7ccbe287
Manas Singha <manassinghaofficial@gmail.com>
2026-05-22 23:24:49 +0530

    Inital Commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <nav className="container"> 
+        <div className="logo">
+          <img
+            src="./images/logo-image.png"
+            width="76px"
+            height="42.75px"
+            alt="Logo"
+            srcset=""
+          />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
